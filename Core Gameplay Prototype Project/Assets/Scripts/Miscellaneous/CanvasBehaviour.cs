f9726c615fbe820eb5e6e9dbfb2fb6cd220e329b
wichapas <wpichetp@ucsc.edu>
2024-02-21 03:29:21 -0800

    second time doin splash

---
+++ Core Gameplay Prototype Project/Assets/Scripts/Miscellaneous/CanvasBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class CanvasBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject teamLogo;
+
+    public GameObject textCrawl;
+    private CanvasGroup image; 
+    void Start()
+    {
+        image = GetComponent<CanvasGroup>();
+        textCrawl.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            teamLogo.SetActive(false);
+            textCrawl.SetActive(true);
+        }
+        else if(image.alpha == 1){
+            teamLogo.SetActive(false);
+            textCrawl.SetActive(true);
+        }    
+    }
+}
---
+++ Core Gameplay Prototype Project/Assets/Scripts/Miscellaneous/TextCrawlBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextCrawlBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int textOutOfScreen = 900;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            SceneManager.LoadScene("MainMenu");
+        }
+        else if(gameObject.transform.position.y >= textOutOfScreen){
+            SceneManager.LoadScene("MainMenu");
+        }   
+    }
+}
