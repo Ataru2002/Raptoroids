b46b82ba0008169b1c50cb7d75e81ac44ead6265
Poom P <sphunjam@ucsc.edu>
2024-02-29 14:58:45 -0800

    Replaced health text with health bar

--- Core Gameplay Prototype Project/Assets/Scripts/Combat/PlayerHealth.cs
+++ Core Gameplay Prototype Project/Assets/Scripts/Combat/PlayerHealth.cs
@@ -7,14 +7,14 @@ public class PlayerHealth : MonoBehaviour, IBulletHittable
     [SerializeField] int maxHealth = 0;
     int currentHealth = 0;
     [SerializeField] ParticleSystem impactParticles;
-    public HealthUI healthUIText;
+    PlayerHealthUI healthUI;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         impactParticles = GetComponentInChildren<ParticleSystem>();
-        healthUIText = GetComponent<HealthUI>();
+        healthUI = FindFirstObjectByType<PlayerHealthUI>();
     }
 
     // Update is called once per frame
@@ -29,7 +29,7 @@ public class PlayerHealth : MonoBehaviour, IBulletHittable
 
         impactParticles.Emit(5);
         currentHealth -= 1;
-        healthUIText.updateHealth(currentHealth);
+        healthUI.UpdateHealth((float)currentHealth / maxHealth);
         if (currentHealth <= 0 && CombatStageManager.Instance != null)
         {
             gameObject.SetActive(false);
---
+++ Core Gameplay Prototype Project/Assets/Scripts/Combat/PlayerHealthUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthUI : MonoBehaviour
+{
+    RectTransform healthBarRect;
+    Image healthBarImg;
+
+    [SerializeField] Color nominalColor;
+    [SerializeField] Color warningColor;
+    [SerializeField] Color criticalColor;
+
+    const float healthBarWidth = 30;
+    const float healthBarHeight = 200;
+
+    private void Awake()
+    {
+        healthBarRect = GetComponent<RectTransform>();
+        healthBarImg = GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void UpdateHealth(float healthRatio)
+    {
+        healthBarRect.sizeDelta = new Vector2(healthBarWidth, healthBarHeight * healthRatio);
+
+        if (healthRatio > 0.6f)
+        {
+            healthBarImg.color = nominalColor;
+        }
+        else if (healthRatio > 0.3f)
+        {
+            healthBarImg.color = warningColor;
+        }
+        else
+        {
+            healthBarImg.color = criticalColor;
+        }
+    }
+}
