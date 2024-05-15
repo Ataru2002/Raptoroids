0d0c66db37d3c1ea4c95973042bbe3d1f42f12e6
Jane Tran <ttran161@ucsc.edu>
2024-05-15 09:20:44 -0700

    implement hill obstacle

--- Core Gameplay Prototype Project/Assets/Hill.cs
+++ Core Gameplay Prototype Project/Assets/Hill.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class Hill : MonoBehaviour
 {
-    public float fallSpeed = 3f;  // speed of the hill falling
-    private float timer;
     public GameObject hillPrefab;
+    public GameObject crackingPrefab;
+    private GameObject crackingInstance;
 
-    void Update()
+    void Start()
     {
-        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+        // Start the coroutine to handle the delayed appearance of the cracking object
+        StartCoroutine(HandleHillCracking());
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -19,5 +20,30 @@ public class Hill : MonoBehaviour
             health.OnBulletHit();
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator HandleHillCracking()
+    {
+        print("cracking appear");
+
+        yield return new WaitForSeconds(3.0f);
+
+        // Determine a random position in the bottom half of the screen horizontally
+        Vector3 bottomHalfPosition = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(0.25f, 0.75f), 0.25f, 10));
+
+        crackingInstance = Instantiate(crackingPrefab, bottomHalfPosition, Quaternion.identity);
+        crackingInstance.transform.localScale = new Vector3(crackingInstance.transform.localScale.x / 2, crackingInstance.transform.localScale.y, crackingInstance.transform.localScale.z);
+
+        // Make the cracking object blink 3 times
+        for (int i = 0; i < 3; i++)
+        {
+            crackingInstance.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+            crackingInstance.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+        }
+        Destroy(crackingInstance);
+        GameObject newHill = Instantiate(hillPrefab, crackingInstance.transform.position, Quaternion.identity);
+        Destroy(newHill, 4.0f);
     }
 }
