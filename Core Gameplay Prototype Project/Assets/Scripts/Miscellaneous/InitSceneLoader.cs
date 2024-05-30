668d148eba161adeb34f77114be3eec7fdc462d7
Poom P <sphunjam@ucsc.edu>
2024-05-30 01:53:43 -0700

    Summarized privacy policy added for Android release

--- Core Gameplay Prototype Project/Assets/Scripts/Miscellaneous/InitSceneLoader.cs
+++ Core Gameplay Prototype Project/Assets/Scripts/Miscellaneous/InitSceneLoader.cs
@@ -5,11 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class InitSceneLoader : MonoBehaviour
 {
+    [SerializeField] GameObject privacyPolicyDisplay;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Testing purposes: force start at tutorial
-        //PlayerPrefs.SetInt("TutorialComplete", 0);
+        if (!PlayerPrefs.HasKey("PrivacyPolicyAccepted"))
+        {
+            PlayerPrefs.SetInt("PrivacyPolicyAccepted", 0);
+        }
+
+        if (PlayerPrefs.GetInt("PrivacyPolicyAccepted") == 0 && Application.platform == RuntimePlatform.Android)
+        {
+            DisplayPrivacyPolicy();
+        }
+        else
+        {
+            BeginGame();
+        }
+    }
+
+    void DisplayPrivacyPolicy()
+    {
+        privacyPolicyDisplay.SetActive(true);
+    }
+
+    public void BeginGame()
+    {
+        PlayerPrefs.SetInt("PrivacyPolicyAccepted", 1);
         SceneManager.LoadScene(1);
+    }
+
+    public void GoToPrivacyPolicySite()
+    {
+        Application.OpenURL("https://sirapatp257.github.io/RaptoroidsPrivacyPolicy/");
     }
 }
